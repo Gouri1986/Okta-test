61183948c006ac56334d297f4f9233912a02e7e4
Gouri <gouri@banyandata.com>
2022-01-10 20:48:41 +0530

    scan files modified

--- src/shared/components/common/alerts/scanFail/ScanFail.jsx
+++ src/shared/components/common/alerts/scanFail/ScanFail.jsx
@@ -5,7 +5,7 @@ import fail from '../../../../assets/images/icons/fail.svg';
 
 export default function ScanFail() {
     return (
-        <div className="width100 flex-r-jc-ac flex-jc-sp-btn bg-red f-white font-14 fw-600">
+        <div className="w-100 flex-r-jc-ac flex-jc-sp-btn bg-danger fc-tertiary f-14 fw-600">
             <div className="pt-18 pb-18 pl-60 flex-r">
                 <div><img src={fail} alt="Scan Fail"></img></div>
                 <div className="ml-15">Successfully Scanned the directory</div>
