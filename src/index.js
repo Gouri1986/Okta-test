67f7064d82e3d28009b7d9716e97a6437f3a74e1
Gouri <gouri@banyandata.com>
2022-01-11 11:34:07 +0530

    style sheet added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './index.scss';
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
