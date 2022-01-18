908a553ff5b8c36143df85a84325d19402fa3153
Pradeep Annadurai <pradeep@banyandata.com>
2022-01-18 17:08:23 +0530

    .

--- src/shared/components/common/tableContainer/table/TableSettings.jsx
+++ src/shared/components/common/tableContainer/table/TableSettings.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import ColumnSettingsIcon from "../columnSettings/ColumnSettings";
 import Refresh from "../refresh/Refresh";
 import Download from "../download/Download";
-import { AddNewIcon } from "./assets/index";
 import { AddNewIcon, FilterIcon } from "./assets/index";
 
 function TableSettings() {
   return (
-    <div
-      style={{ borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
-      className='flex-r-ac flex-jc-sp-btn p-30 pos-sk t-0'
-    >
+    <div className='flex-r-ac flex-jc-sp-btn bg-white p-25'>
       <div className='flex-c'>
         <div className='f-13 fw-500 fc-quaternary'>Filter</div>
         <div className='bdr-buttom-primary-1 w-600'>
