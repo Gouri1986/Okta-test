964c9583e877a0e5b654d60170c567ae77ef8c9f
Pradeep Annadurai <pradeep@banyandata.com>
2022-01-18 08:49:48 +0530

    table settings

--- src/shared/components/common/tableContainer/table/TableSettings.jsx
+++ src/shared/components/common/tableContainer/table/TableSettings.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import ColumnSettingsIcon from '../columnSettings/ColumnSettings';
-import Refresh from '../refresh/Refresh';
-import Download from '../download/Download';
+import React from "react";
+import ColumnSettingsIcon from "../columnSettings/ColumnSettings";
+import Refresh from "../refresh/Refresh";
+import Download from "../download/Download";
 
 function TableSettings() {
-    return (
-        <>
-            <div className='flex-r-ac flex-jc-sp-btn'>
-                <div className='Flex-c'>
-                    <div>Filter</div>
-                    <div className='flex-r'>
-                        <div>Filter Icon</div>
-                        <div>filter</div>
-                    </div>
-                </div>
-                <div className='flex-r flex-jc-sp-btn'>
-                    <div><ColumnSettingsIcon /></div>
-                    <div><Refresh /></div>
-                    <div><Download /></div>
-                    <div>Add more Icon</div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div className='flex-r-ac flex-jc-sp-btn p-30 pos-sk t-0'>
+      <div className='flex-c'></div>
+      <div className='flex-r-ac flex-jc-sp-btn'>
+        <div className='mr-30'>
+          <ColumnSettingsIcon />
+        </div>
+        <div className='mr-30'>
+          <Refresh />
+        </div>
+        <div>
+          <Download />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TableSettings
+export default TableSettings;
