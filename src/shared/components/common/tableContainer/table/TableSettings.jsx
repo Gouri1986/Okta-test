266879429e54b430e63fd9e9b5b7fba8e2de27bc
Gouri <gouri@banyandata.com>
2022-01-18 11:42:19 +0530

    add new button added inside the table container

--- src/shared/components/common/tableContainer/table/TableSettings.jsx
+++ src/shared/components/common/tableContainer/table/TableSettings.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ColumnSettingsIcon from "../columnSettings/ColumnSettings";
 import Refresh from "../refresh/Refresh";
 import Download from "../download/Download";
+import {AddNewIcon} from './assets/index';
 
 function TableSettings() {
   return (
     <div className='flex-r-ac flex-jc-sp-btn p-30 pos-sk t-0'>
       <div className='flex-c'></div>
       <div className='flex-r-ac flex-jc-sp-btn'>
+        <div className='mr-30'>
+          <button className="bg-white no-bdr"><AddNewIcon /></button>
+        </div>
         <div className='mr-30'>
           <ColumnSettingsIcon />
         </div>
