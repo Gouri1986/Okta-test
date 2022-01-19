745f5995515163add97d43f061a306c66061f652
Pradeep Annadurai <pradeep@banyandata.com>
2022-01-19 18:06:32 +0530

    filter search

--- src/shared/components/common/tableContainer/filter/Filter.jsx
+++ src/shared/components/common/tableContainer/filter/Filter.jsx
@@ -12,34 +12,14 @@ const Filter = ({
   showFilterDrawer,
 }) => {
   const { header } = tbData;
-  const quickFilterContents = header?.filter(
-    (e) =>
-      e.id === "service-type" ||
-      e.id === "resource-type" ||
-      e.id === "regular-control"
-  );
 
   return (
     <div className='titan-filter'>
-      <div
-        onClick={() => {
-          showFilterDrawer(!filterDrawer);
-          showReport(false);
-        }}
-        className='titan-filter-icon'
-      >
-        <FilterIcon />
-      </div>
-      <div className='dropdown-container'>
-        {quickFilterContents?.map((content) => (
-          <div className='titan-filter-dropdown'>
-            <FilterDropDown
-              setTableContents={setTableContents}
-              {...content}
-              tableData={tbData}
-            />
-          </div>
-        ))}
+      <div className='filter-search'>
+        <span className='filter-search-icon'>
+          <FilterIcon />
+        </span>
+        <input className='filter-search-input' />
       </div>
     </div>
   );
--- src/shared/components/common/tableContainer/table/TableSettings.jsx
+++ src/shared/components/common/tableContainer/table/TableSettings.jsx
@@ -3,14 +3,15 @@ import ColumnSettingsIcon from "../columnSettings/ColumnSettings";
 import Refresh from "../refresh/Refresh";
 import Download from "../download/Download";
 import { AddNewIcon, FilterIcon } from "./assets/index";
+import Filter from "../filter/Filter";
 
 function TableSettings() {
   return (
     <div className='flex-r-ac flex-jc-sp-btn p-25'>
       <div className='flex-c'>
         <div className='f-13 fw-500 fc-quaternary'>Filter</div>
-        <div className='bdr-buttom-primary-1 w-600'>
-          <FilterIcon />
+        <div className=' w-600'>
+          <Filter />
         </div>
       </div>
       <div className='flex-r-ac flex-jc-sp-btn'>
