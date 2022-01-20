8de144eb6456bc3d1e82833c7720073d8a19720f
jobsonpvarghese <jobsonvarghese@gmail.com>
2022-01-20 15:55:46 +0530

    Modal props changed with table title

--- src/shared/layout/MetadataLayout.jsx
+++ src/shared/layout/MetadataLayout.jsx
@@ -29,7 +29,7 @@ const Layout = props => {
         <div className="flex-c ml-30 mr-30 hvh-81">
           <div className="flex-c bdr-r-10 bg-white">
             <div>
-              <TableSettings modalOnClick={() => setOpen(!open)}/>
+              <TableSettings modalOnClick={() => setOpen(!open)} />
             </div>
             <div className="overflow-x-scroll metadata-table-container">
               {tableData.data?.length > 0 ? (
@@ -50,12 +50,16 @@ const Layout = props => {
         open={open}
         close={() => setOpen(false)}
         size="lg" // sm, md, lg, xl
-        body={` Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem Ipsum`}
+        body={
+          <div>
+            <h3>{tableTitle}</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, ratione. Aliquam inventore,
+              vel fuga repudiandae officia, incidunt, esse eius eveniet optio sit nemo quasi a aperiam
+              temporibus. Architecto, amet quos.
+            </p>
+          </div>
+        }
       />
     </div>
   )
