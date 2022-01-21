83ca7ab72a0bf1a8f1f684de50d3f8a26cb1e2f7
jobsonpvarghese <jobsonvarghese@gmail.com>
2022-01-21 18:40:41 +0530

    Modal form (textbox, textarea done )with dynamic input render done

---
+++ src/shared/components/common/inputs/input/TextBox.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import "./TextBox.scss"
+
+const TextBox = props => {
+  const { label, id, type, row, madatory, value, defaultValue, placeholder, onChange, handleClear } = props
+
+  return (
+    <div>
+      <label className="text-box-label">{label}</label>
+      {madatory !== undefined && madatory === true ? <span>*</span> : ""}
+      {type === "text" ? (
+        <input
+          className="text-box mt-6"
+          id={id}
+          placeholder={placeholder}
+          type="text"
+          name={id}
+          // defaultValue={value}
+          //value={value}
+          onChange={onChange}
+        />
+      ) : type === "textarea" ? (
+        <textarea
+          className="textarea mt-6"
+          id={id}
+          placeholder={placeholder}
+          rows={row}
+          onChange={onChange}
+        ></textarea>
+      ) : (
+        ""
+      )}
+    </div>
+  )
+}
+
+export default TextBox
