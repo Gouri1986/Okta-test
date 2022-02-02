69da6fb7bab419d134742239a5b772b9880e6511
jobsonpvarghese <jobsonvarghese@gmail.com>
2022-02-02 20:37:13 +0530

    New version 2 pagination functionality done & final style penfing

--- src/shared/components/common/tableContainer/pagination/Pagination/Pagination.jsx
+++ src/shared/components/common/tableContainer/pagination/Pagination/Pagination.jsx
@@ -13,15 +13,26 @@ const PaginationComponent = props => {
       <div className="flex-r flex-jc-sp-btn">
         <div className="rows-per-page-box">
           <label>Rows per page</label>
-          <select className="row-dropdown ml-15 " onChange={e => setRowsPerPage(e.target.value)}>
+          <select
+            className="row-dropdown ml-15 "
+            onChange={e => {
+              setRowsPerPage(e.target.value)
+              setPage(1)
+            }}
+          >
             <option value={10}>10</option>
             <option value={25}>25</option>
             <option value={50}>50</option>
             <option value={100}>100</option>
           </select>
         </div>
-        <div>
-          <Pagination shape="rounded" count={totalPage} onChange={(event, val) => setPage(val)} />
+        <div className="pagination-box">
+          <Pagination
+            size="small"
+            shape="rounded"
+            count={totalPage}
+            onChange={(event, val) => setPage(val)}
+          />
         </div>
       </div>
     </>
