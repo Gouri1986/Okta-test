c38608da4cb41231922c6c6a4a45e77b89f03326
Gouri <gouri@banyandata.com>
2022-02-02 19:59:35 +0530

    breadcrumb style updated as per new design

--- src/shared/components/common/breadcumbs/Breadcrumbs.jsx
+++ src/shared/components/common/breadcumbs/Breadcrumbs.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import { Breadcrumbs, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+// import { Link } from 'react-router-dom';
 
 import './Breadcrumbs.scss';
-import HomeIcon from './HomeIcon';
+import {HomeIcon, BreadcrumbSeperatorIcon} from './assets';
+
+
+
+import Link from '@mui/material/Link';
 
 const BreadCrumbs = (props) => {
   const { parentTitle, parentPath, tableTitle1, tableTitle2 } = props;
   return (
     <div>
       <Breadcrumbs
-        separator={<NavigateNextIcon fontSize='medium' />}
-        aria-label='breadcrumb'
-      >
-        <Typography key={1} color='text.primary'>
+        separator={<BreadcrumbSeperatorIcon />}
+        aria-label='breadcrumb'>
+        <Typography key={1}>
           <Link to={parentPath}>
             {parentTitle === 'Dashboard' ? <HomeIcon size={16} /> : parentTitle}
           </Link>
         </Typography>
-        <Typography key={2} color='text.primary'>
+        <Typography key={2}
+          color="#080430"
+          fontSize="16px"
+          fontWeight="500"
+        >
           {tableTitle1}
         </Typography>
-        <Typography key={3} color='text.primary'>
+        <Typography key={3}
+          color="#6C6C6C"
+          fontSize="16px"
+          fontWeight="500"
+        >
           {tableTitle2}
         </Typography>
       </Breadcrumbs>
