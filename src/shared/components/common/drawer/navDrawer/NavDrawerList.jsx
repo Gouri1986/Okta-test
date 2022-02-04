01e5b5378b1ddd6f411269345743831db419ca47
Pradeep Annadurai <pradeep@banyandata.com>
2022-02-04 10:19:05 +0530

    changes nav menu font color from tertiary to white

--- src/shared/components/common/drawer/navDrawer/NavDrawerList.jsx
+++ src/shared/components/common/drawer/navDrawer/NavDrawerList.jsx
@@ -292,7 +292,7 @@ const DrawerList = (props) => {
                   <li className='flex-c mb-10'>
                     <div
                       onClick={() => onSecondMenuClick(item)}
-                      className={`flex-r-ac flex-jc-sp-btn cp f-14 fw-400 lh-2-1 fc-tertiary  flex-r-ac pl-15 pr-15 pt-10 pb-10 ${
+                      className={`flex-r-ac flex-jc-sp-btn cp f-14 fw-400 lh-2-1 fc-white  flex-r-ac pl-15 pr-15 pt-10 pb-10 ${
                         secondMenuItemSelected.bool &&
                         secondMenuItemSelected.title === item.title
                           ? "second-menu-route-list-title-selected"
