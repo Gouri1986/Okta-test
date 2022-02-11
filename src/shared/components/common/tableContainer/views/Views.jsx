5df84de4442ef11b74d1d065590a94157eafa6d7
Gouri <gouri@banyandata.com>
2022-02-11 15:10:56 +0530

    view tab is in Progress

---
+++ src/shared/components/common/tableContainer/views/Views.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Views() {
+  return (
+    <>
+        <div className='flex-r'>
+            <div className='fw-600 f-14 fc-secondary'>View<span> Complaince </span>by</div>
+            <div></div>
+        </div>
+    </>
+  )
+}
+
+export default Views
+
+
--- src/shared/components/common/tenant/Tenant.jsx
+++ src/shared/components/common/tenant/Tenant.jsx
@@ -5,8 +5,8 @@ function Tenant() {
   return(
     <>
        <div className='flex-r'>
-           <p className=''>Cloud Tenant ID:</p>
-           <span className='ml-2 fc-tertiary'>bds6445934735</span>
+           <p className='ml-5 fc-quaternary fw-500'>Cloud Tenant ID:</p>
+           <span className='ml-5 fc-tertiary'>bds6445934735</span>
            <div className='ml-10 mr-5'>
                <img src={greyarrow}></img>
            </div>
