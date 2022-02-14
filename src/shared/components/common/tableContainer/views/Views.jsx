90f2efa1f5aa106a4f382eedfe50dcc65770f254
Gouri <gouri@banyandata.com>
2022-02-14 11:19:49 +0530

    view tab added

--- src/shared/components/common/tableContainer/views/Views.jsx
+++ src/shared/components/common/tableContainer/views/Views.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 function Views() {
   return (
     <>
-        <div className='flex-r'>
-            <div className='fw-600 f-14 fc-secondary'>View<span> Complaince </span>by</div>
-            <div></div>
+        <div className='flex-r-ac'>
+            <div className='fw-600 f-14 fc-secondary mr-10'>View<span> Complaince </span>by</div>
+            <div className='bdr-r-10 bg-violet-container shw-violet p-5 flex-r'>
+              <div className='switch pl-15 pr-15 pt-5 pb-5 mr-5'>Control ID</div>
+              <div className='switch pl-15 pr-15 pt-5 pb-5'>Resource ID</div>
+            </div>
         </div>
     </>
   )
