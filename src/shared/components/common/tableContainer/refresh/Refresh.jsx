f9c41b9f2770d63e7e9fe53bd3f250a029cc0bad
Gouri <gouri@banyandata.com>
2022-02-14 15:42:45 +0530

    _hover.scss added

--- src/shared/components/common/tableContainer/refresh/Refresh.jsx
+++ src/shared/components/common/tableContainer/refresh/Refresh.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { RefreshIcon } from "./assets";
-import "./refresh.scss";
 
 const Setting = () => {
   return (
-    <div className='refresh-icon-button pt-7 pl-7 pr-7 cp bdr-r-6'>
+    <div className='icon-hovr bg-white pt-7 pl-7 pr-7 cp bdr-r-6'>
       <RefreshIcon />
     </div>
   );
