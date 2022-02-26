da2f06b33d61104c3066113a09f179532c718424
Gouri <gouri@banyandata.com>
2022-02-26 11:19:51 +0530

    ui of control is fixed

--- src/routes/featureRoutes/complianceDashboard.js
+++ src/routes/featureRoutes/complianceDashboard.js
@@ -176,7 +176,7 @@ const AWS = [
       {
         title: "Control",
         id: "bcGcpControlDescription",
-        width: 350
+        width: 370
       },
       {
         title: "Compliance Status",
@@ -365,12 +365,12 @@ const routes = [
     headerStatic: [
       {
         title: "CT Group",
-        id: " ",
+        id: "bcCtgName",
         width: 120
       },
       {
         title: "Cloud Tenant",
-        id: " ",
+        id: "bcCtName",
         width: 150
       },
       {
